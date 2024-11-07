a119d2c82689f999bee30ea46d02f565edae0153
Sabina Cebreroz Silva <sabina.cebreroz@wizeline.com>
2024-11-07 00:11:15 -0600

    Relocating Postman Collections

--- tests/ui/steps/project_steps.py
+++ tests/ui/steps/project_steps.py
@@ -5,20 +5,20 @@ fake = Faker()
 
 
 @Step("the user click the new project button")  # type: ignore
-def step_click_the_add_task_button(context):
+def step_click_the_add_project_button(context):
     context.project_page = context.page_factory.get_page("project")
     # context.new_project_page.load()
     context.project_page.click_add_project_button()
 
 
 @Step("the user create a project")  # type: ignore
-def step_create_a_task(context):
+def step_create_a_project(context):
     context.project_name = fake.country()
     context.project_page.create_a_project(context.project_name)
 
 
 @Step("the user validates the project data")  # type: ignore
-def step_validate_the_task(context):
+def step_validate_the_project(context):
     from selenium.webdriver.common.by import By
 
     txt_project_name = (By.XPATH, f"//H1[text()='{context.project_name}']")
